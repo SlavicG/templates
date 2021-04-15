df4765a3a30d825ac2efee4d778568521b2724f0
SlavicG <68696994+SlavicG@users.noreply.github.com>
2021-04-15 11:48:50 +0300

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -2,20 +2,32 @@ template<typename T>
 struct fenwick_tree{
 	int n;
 	vector<T> fen;
-
+	int LG;
+	void compute_log(int n)
+	{
+		LG = 1;
+		while(n){
+			++LG;
+			n /= 2;
+		}
+	}
 	fenwick_tree(int _n){
 		n = _n + 5;
+		int x = n;
 		fen.assign(n + 5, 0);
+		compute_log(n);
 	}
 	fenwick_tree(vector<ll> a){
 		n = (int)a.size() + 5;
 		fen.assign(n + 5, 0);
 		build(a);
+		compute_log(n);
 	}
 	fenwick_tree(vector<int> a){
 		n = (int)a.size() + 5;
 		fen.assign(n + 5, 0);
 		build(a);
+		compute_log(n);
 	}
 
 	void build(vector<int> a){
@@ -51,5 +63,19 @@ struct fenwick_tree{
 	}
 	T query(int l, int r){
 		return query(r) - query(l - 1);
+	}
+
+	T fen_search(T v)
+	{
+		T sum = 0;
+		int pos = 0;
+		for(int i = LG;i >= 0;i--)
+		{
+			if(pos + (1 << i) < n && sum + fen[pos + (1 << i)] < v){
+				sum += fen[pos + (1 << i)];
+				pos += (1 << i);
+			}
+		}
+		return pos + 1;
 	}
 };
