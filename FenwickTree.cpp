7e077b0ca8a5460eff03b2625ef2291900fbb397
SlavicG <68696994+SlavicG@users.noreply.github.com>
2021-04-21 11:52:37 +0300

    Update FenwickTree.cpp

--- FenwickTree.cpp
+++ FenwickTree.cpp
@@ -1,81 +1,36 @@
-template<typename T>
-struct fenwick_tree{
-	int n;
-	vector<T> fen;
-	int LG;
-	void compute_log(int n)
-	{
-		LG = 1;
-		while(n){
-			++LG;
-			n /= 2;
-		}
-	}
-	fenwick_tree(int _n){
-		n = _n + 5;
-		int x = n;
-		fen.assign(n + 5, 0);
-		compute_log(n);
-	}
-	fenwick_tree(vector<ll> a){
-		n = (int)a.size() + 5;
-		fen.assign(n + 5, 0);
-		build(a);
-		compute_log(n);
-	}
-	fenwick_tree(vector<int> a){
-		n = (int)a.size() + 5;
-		fen.assign(n + 5, 0);
-		build(a);
-		compute_log(n);
-	}
+struct fenwick_tree {
+    vector<ll> fen;
+    int n;
+    fenwick_tree(int n){
+        this->n = n;
+        fen.assign(n, 0);
+    }
 
-	void build(vector<int> a){
-		for(int i = 0;i < (int)a.size();i++){
-			upd(i + 1, a[i]);
-		}
-	}
-	void build(vector<ll> a){
-		for(int i = 0;i < (int)a.size();i++){
-			upd(i + 1, a[i]);
-		}
-	}
-
-	void upd(int poz, int val)
-	{
-		for(int i = poz;i < n;i += (i & (-i))){
-			fen[i] += val;
-		}
-	}
-	void upd(int poz, ll val)
-	{
-		for(int i = poz;i < n;i += (i & (-i))){
-			fen[i] += val;
-		}
-	}
-
-	T query(int poz){
-		T ans = 0;
-		for(int i = poz;i > 0;i -= (i & (-i))){
-			ans += fen[i];
-		}
-		return ans;
-	}
-	T query(int l, int r){
-		return query(r) - query(l - 1);
-	}
-
-	T fen_search(T v)
-	{
-		T sum = 0;
-		int pos = 0;
-		for(int i = LG;i >= 0;i--)
-		{
-			if(pos + (1 << i) < n && sum + fen[pos + (1 << i)] < v){
-				sum += fen[pos + (1 << i)];
-				pos += (1 << i);
-			}
-		}
-		return pos + 1;
-	}
+    template<typename T>
+    fenwick_tree(vector<T> a){
+    	this->n = sz(a);
+    	fen.assign(n, 0);
+        for (size_t i = 0; i < sz(a); i++)
+            modif(i, a[i]);
+    }
+    ll query(int r){
+        ll ret = 0;
+        while(r >= 0){
+        	ret += fen[r];
+        	r = (r & (r + 1)) - 1;
+        }
+        return ret;
+    }
+    ll query(int l, int r) {
+    	ll ret = query(r);
+    	if(l)ret -= query(l - 1);
+        return ret;
+    }
+    void modif(int idx, int val){
+    	while(idx < n)
+    	{
+    		fen[idx] += val;
+    		idx = idx | (idx + 1);
+    	}
+    }
 };
